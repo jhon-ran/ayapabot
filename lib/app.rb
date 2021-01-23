da31416f374c9e51870a6f01ff54c0a6995d2028
Jordan <jordancaley10@gmail.com>
2021-01-23 13:00:49 +0100

    Like the last 20 tweets, not including our own

--- lib/app.rb
+++ lib/app.rb
@@ -11,10 +11,18 @@ client = Twitter::REST::Client.new do |config|
 end
 
 # Tweet action
-#client.update("gwübü'k tzu'ndyi!")
+# client.update("gwübü'k tzu'ndyi!")
 
-# Like the 25 most recent tweets with the word "ayapaneco"
-tweets = client.search("ayapaneco", result_type: "recent").take(5)
+# Like the 20 most recent tweets with the word "ayapaneco"
+tweets = client.search("ayapaneco", result_type: "recent").take(20)
+
+# Don't like our own tweets
+i = 0
+while i < tweets.length
+  if tweets[i].user.screen_name == "ayapaneco"
+    tweets.delete_at(i)
+  end
+  i = i + 1
+end
+
 client.favorite(tweets)
-
-# New comment
