3fbbe35aa88872086fc7de8c6ecdf3fe102de591
Jordan <jordancaley10@gmail.com>
2021-01-26 13:17:36 +0100

    Add word_of_the_day program

---
+++ lib/word_of_the_day.rb
@@ -0,0 +1,21 @@
+require 'bundler'
+Bundler.require
+
+Dotenv.load
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+  config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+  config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+  config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+end
+
+table = CSV.parse(File.read("db/dictionary.csv"), headers: true).map(&:fields)
+
+word_of_the_day = table.sample
+
+if word_of_the_day[2] == '*'
+  client.update("La palabra del día es #{word_of_the_day[1]} --> '#{word_of_the_day[0]}'")
+else
+  client.update("La palabra del día es #{word_of_the_day[1]} o #{word_of_the_day[2]} --> '#{word_of_the_day[0]}'")
+end
