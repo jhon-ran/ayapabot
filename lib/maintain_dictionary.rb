27fc2a01e53d931e153ff713aa5883ad11bdbd16
Jordan <jordancaley10@gmail.com>
2021-01-29 11:19:18 +0100

    Comment code

--- lib/maintain_dictionary.rb
+++ lib/maintain_dictionary.rb
@@ -3,20 +3,13 @@ Bundler.require
 
 # Converts csv table into an array
 table = CSV.parse(File.read("db/dictionary.csv"), headers: true).map(&:fields)
+
+# Change order of columns 4 and 5
 i = 0
 while i < table.length 
   table[i][3], table[i][4] = table[i][4], table[i][3]
   i = i + 1
 end
 
+# Write new table to csv (this didn't include headers so we added them back in manually in csv)
 File.write("db/dictionary.csv", table.map(&:to_csv).join)
-
-
-
-# n = 0
-# while n < table.length
-#   CSV.open("db/dictionary.csv", "w") do |csv|
-#     csv << table[n]
-#   end
-#   n = n + 1
-# end
