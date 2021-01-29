0164d7bc94a828aa5da59e926caa0cfbde250406
Jordan <jordancaley10@gmail.com>
2021-01-29 16:09:04 +0100

    Sort words alphabetically

--- lib/maintain_dictionary.rb
+++ lib/maintain_dictionary.rb
@@ -2,14 +2,17 @@ require 'bundler'
 Bundler.require
 
 # Converts csv table into an array
-table = CSV.parse(File.read("db/dictionary.csv"), headers: true).map(&:fields)
+table = CSV.parse(File.read("db/dictionary.csv"), headers: true)
 
-# Change order of columns 4 and 5clear
-i = 0
-while i < table.length 
-  table[i][3], table[i][4] = table[i][4], table[i][3]
-  i = i + 1
-end
+# # Change order of columns 4 and 5
+# i = 0
+# while i < table.length 
+#   table[i][3], table[i][4] = table[i][4], table[i][3]
+#   i = i + 1
+# end
 
-# Write new table to csv (this didn't include headers so we added them back in manually in csv)
-File.write("db/dictionary.csv", table.map(&:to_csv).join)
+# # Write new table to csv (this didn't include headers so we added them back in manually in csv)
+# File.write("db/dictionary.csv", table.map(&:to_csv).join)
+
+# Sort words alphabetically (but removes headers!)
+File.write("db/dictionary.csv", table.sort_alphabetical.map(&:to_csv).join)
